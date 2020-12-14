c2dc9575234740752d27b4615b11e4e82de8c66f
Ashy6 <z1801273437@163.com>
2020-12-14 11:10:17 +0800

    返回顶部

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <!-- 进行路由渲染 -->
     <router-view></router-view>
+      <!-- <el-backtop target=".page-component__scroll .el-scrollbar__wrap"></el-backtop> -->
   </div>
 </template>
 
