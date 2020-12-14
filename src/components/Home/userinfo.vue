c923e6dd63754dc8a1c297ee143baaa2e302132f
Ashy6 <z1801273437@163.com>
2020-12-14 11:29:44 +0800

    用户信息页面

---
+++ src/components/Home/userinfo.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>
+            用户信息管理
+        </h1>
+        <p>基本信息</p>
+        <p>详细信息</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
