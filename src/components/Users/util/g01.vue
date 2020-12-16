e2f5be468fe040054ec2fc8ccc80fbfe594701a7
Ashy6 <z1801273437@163.com>
2020-12-16 08:10:09 +0800

    体脂

--- src/components/Users/util/g01.vue
+++ src/components/Users/util/g01.vue
@@ -6,35 +6,35 @@
     <el-form class="login_form" label-width="80px">
       <!-- 用户名 -->
       <el-input
-        placeholder="请输入身高米数"
+        placeholder="请输入身高(cm)"
         type="stature"
-        v-model="bodyFat.stature"
+        v-model="body.stature"
       ></el-input
       ><br />
       <el-input
         placeholder="请输入体重(kg)"
         type="weight"
-        v-model="bodyFat.weight"
+        v-model="body.weight"
       ></el-input
       ><br />
       <el-input
-        placeholder="请输入年龄"
+        placeholder="请输入年龄(岁)"
         type="weight"
-        v-model="bodyFat.personage"
+        v-model="body.personage"
       ></el-input>
-      <el-form-item label="您的性别">
-        <el-radio-group v-model="bodyFat.personsex" size="medium">
+      <el-form-item label="您的性别:">
+        <el-radio-group v-model="body.personsex" size="medium">
           <el-radio border label="1">男</el-radio>
           <el-radio border label="0">女</el-radio>
         </el-radio-group>
       </el-form-item>
       <el-form-item class="btns">
-        <button @click="getBodyFat">计算体脂率</button></el-form-item
+        <button @click="getBody">计算体脂率</button></el-form-item
       >
       <el-input
         placeholder="体脂"
         type="bfat"
-        v-model="bodyFat.bfat"
+        v-model="body.bfat"
         disabled
       ></el-input>
       <p>
@@ -46,13 +46,13 @@
         BMI=体重（千克）÷（身高 x 身高）（米）<br />
         体脂率的计算公式是：1.2 x BMI+0.23 x 年龄-5.4-10.8 x 性别
       </p>
-      <p>身高： {{ bodyFat.stature }}</p>
-      <p>体重： {{ bodyFat.weight }}</p>
-      <p>年龄： {{ bodyFat.personage }}</p>
-      <p>性别： {{ bodyFat.personsex }}</p>
-      <p>BMI： {{ bodyFat.bmi }}</p>
-      <p>体脂率： {{ bodyFat.bfat }}</p>
-      <p>根据您的体脂率，您的身材{{ bodyFat.news }}</p>
+      <p>身高： {{ body.stature }}</p>
+      <p>体重： {{ body.weight }}</p>
+      <p>年龄： {{ body.personage }}</p>
+      <p>性别： {{ body.personsex }}</p>
+      <p>BMI： {{ body.bmi }}</p>
+      <p>体脂率： {{ body.bfat }}</p>
+      <!-- <p>根据您的体脂率，您的身材{{ body.news }}</p> -->
       <!-- 按钮 -->
     </el-form>
   </div>
@@ -62,11 +62,12 @@
 export default {
   data() {
     return {
-      bodyFat: {
+      body: {
         stature: "",
         weight: "",
         personage: "",
         personsex: "",
+        // 结果
         bmi: "",
         bfat: "",
         news: "",
@@ -74,36 +75,40 @@ export default {
     };
   },
   created() {
-    // this.bodyFat.stature = JSON.parse(this.bodyFat.stature)
-    // this.form.bd = JSON.stringify(this.bodyFat.stature)
+    // this.body.stature = JSON.parse(this.body.stature)
+    // this.form.bd = JSON.stringify(this.body.stature)
   },
   methods: {
-    getBodyFat() {
+    getBody() {
       var bfat,
         news,
-        personheight = this.bodyFat.stature / 100,
-        personage = this.bodyFat.personage,
-        personweight = this.bodyFat.weight,
-        personSex = this.bodyFat.personsex,
+        personheight = this.body.stature / 100,
+        personage = this.body.personage,
+        personweight = this.body.weight,
+        personSex = this.body.personsex,
         BMI = personweight / (personheight * personheight);
-      console.log(BMI);
-      console.log(personheight);
       var bfat = 1.2 * BMI + 0.23 * personage - 5.4 - 10.8 * personSex;
       bfat = bfat.toFixed(2);
       var minNum = 0.15 + 0.1 * (1 - personSex);
       var maxNum = 0.18 + 0.1 * (1 - personSex);
-      console.log(minNum);
-      console.log(maxNum);
+      // if (bfat > maxNum * 100) {
+      //   news = "偏胖，建议您平时多注意运动，并减少脂肪的摄入";
+      // } else if (bfat < minNum * 100) {
+      //   news = "偏瘦，建议您适当增加脂肪与蛋白质的摄入，并积极进行体育锻炼";
+      // } else {
+      //   news = "合适,继续保持！";
+      // }
       if (bfat > maxNum * 100) {
-        news = "偏胖，建议您平时多注意运动，并减少脂肪的摄入";
+        alert( "根据您的体脂率为"+bfat+"，您的身材偏胖，建议您平时多注意运动，并减少脂肪的摄入");
       } else if (bfat < minNum * 100) {
-        news = "偏瘦，建议您适当增加脂肪与蛋白质的摄入，并积极进行体育锻炼";
+        // alert(bfat+"根据您的体脂率为");
+        alert( "根据您的体脂率为"+bfat+"，您的身材偏瘦，建议您适当增加脂肪与蛋白质的摄入，并积极进行体育锻炼");
       } else {
-        news = "合适,继续保持！";
+        alert( "根据您的体脂率为"+bfat+"，您的身材合适,继续保持！");
       }
-      this.bodyFat.bfat = bfat;
-      this.bodyFat.bmi = BMI;
-      this.bodyFat.news = news;
+      this.body.bfat = bfat;
+      this.body.bmi = BMI;
+      this.body.news = news;
     },
   },
 };
