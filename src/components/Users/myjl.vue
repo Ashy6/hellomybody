8ddd9e8c23ee2b054677d542e9e49bd10252445b
Ashy6 <z1801273437@163.com>
2021-03-07 04:07:14 +0800

    卡路里查询

--- src/components/Users/myjl.vue
+++ src/components/Users/myjl.vue
@@ -1,15 +1,21 @@
 <template>
-    <div>
-        <h3>wode jilu模板</h3>
+  <div class="container">
+    <h3></h3>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>个人中心</el-breadcrumb-item>
+      <el-breadcrumb-item>我的足迹</el-breadcrumb-item>
+      <!-- <el-breadcrumb-item>修改信息</el-breadcrumb-item> -->
+    </el-breadcrumb>
+    <div class="shadow-lg p-3 mb-5 bg-white rounded">
+      <p>即将上线</p>
     </div>
+  </div>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <style lang="less" scoped>
-
 </style>
